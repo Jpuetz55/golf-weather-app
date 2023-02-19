63a9c7e773b6fd045487cc8a0b4de03b97b9ef0a
Jpuetz55 <jpuetz55@gmail.com>
2023-02-19 00:30:06 -0600

    Changed api call to open meteo

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Calendar from '../components/Calendar'
 import moment from 'moment'
-import queryString from 'query-string'
+import queryString, { stringify } from 'query-string'
 import React from 'react'
 
 function Home( { weather } ) {
@@ -13,69 +13,30 @@ function Home( { weather } ) {
     <Calendar data={weather}/>
     </>
   )
+
+  // Send a GET request to the specified endpoint
     
 }
 export async function getServerSideProps() {
-    // Send a GET request to the specified endpoint
-   // get your key from app.tomorrow.io/development/keys
-   const apikey = "KDY75kl3GNk9qv32n5pNGpMsSNqiYCKm";
-   const baseURL = "https://api.tomorrow.io/v4/timelines"
 
-   // pick the location, as a latlong pair
-   let location = [38.8403, 97.6114];
+  const params = [
+  "temperature_2m",
+  "relativehumidity_2m",
+  "dewpoint_2m",
+  "apparent_temperature",
+  "pressure_msl",
+  "surface_pressure",
+  "precipitation,windspeed_10m",
+  "windspeed_80m",
+  "soil_temperature_0_to_10cm"
 
-   // list the fields
-   const fields = [
-   //"sunriseTime",
-   //"sunsetTime",
-   //"rainAccumulation",
-   "precipitationType",
-   "precipitationProbability",
-   //"dewPoint",  
-   //"rainIntensity",
-   //"pressureSurfaceLevel",
-   //"humidity",
-   //"precipitationIntensity",
-   //"precipitationType",
-   "windSpeed",
-   "windGust",
-   //"windDirection",
-   //"temperature",
-   "temperatureApparent",
-   //"cloudCover",
-   //"cloudBase",
-   //"cloudCeiling",
-   //"weatherCode",
-   ];
+  ];
 
-   // choose the unit system, either metric or imperial
-   const units = "imperial";
-
-   // set the timesteps, like "current", "1h" and "1d"
-   const timesteps = ["1d"];
-
-   // configure the time frame up to 6 hours back and 15 days out
-   const now = moment.utc();
-   const startTime = moment.utc(now).add(0, "minutes").toISOString();
-   const endTime = moment.utc(now).add(13, "days").toISOString();
-
-   // specify the timezone, using standard IANA timezone format
-   const timezone = "America/New_York";
-
-   // request the timelines with all the query string parameters as options
-   const getTimelineParameters =  queryString.stringify({
-       location,
-       fields,
-       units,
-       timesteps,
-       startTime,
-       endTime,
-       timezone,
-   }, {arrayFormat: "comma"});   
-
-   const res = await fetch(baseURL + "?" + getTimelineParameters + `&apikey=${apikey}`, {method: "GET"})
-   const data = await res.json()
-   console.log(data)
+  const baseURL = "https://api.open-meteo.com/v1/gfs?latitude=38.8403&longitude=97.6114&hourly="
+  const res = await fetch(baseURL + params.join(","), {method: "GET"});
+  const data = await res.json();
+  console.log(data);
+   
   return {
     props: {
       weather: data,
@@ -84,3 +45,5 @@ export async function getServerSideProps() {
 }
 
 export default Home;
+
+
