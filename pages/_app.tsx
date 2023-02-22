72454f7ea1690fd83defd331d45283d018e52ec6
Jpuetz55 <jpuetz55@gmail.com>
2023-02-21 21:23:33 -0600

    ...pageProps?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import '../components/Calendar.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 
-function MyApp({ Component, pageProps: {...pageProps} }) {
+function MyApp({ Component, pageProps}) {
   return (
     <>
     <Navbar/>
