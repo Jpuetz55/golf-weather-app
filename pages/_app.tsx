d605a5a34ac9fe59a5f97b1ff3eecc47338e53c7
Jpuetz55 <jpuetz55@gmail.com>
2023-02-21 21:10:58 -0600

    removed next files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,18 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import '../components/Navbar.css'
 import '../components/Calendar.css'
-import { SessionProvider, useSession } from "next-auth/react";
 
 import type { AppProps } from 'next/app'
 import React from 'react'
-import { Session } from 'inspector';
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+function MyApp({ Component, pageProps: {...pageProps} }) {
   return (
-    <SessionProvider session={session}>
+    <>
     <Navbar/>
     <Component {...pageProps} />
-    </SessionProvider>
+    </>
+    
+    
   )
 }
 
