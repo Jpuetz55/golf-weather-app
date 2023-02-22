7b8e783ca5e2056ee6b7f966e471e85e12b57b3f
Jpuetz55 <jpuetz55@gmail.com>
2023-02-21 21:17:34 -0600

    removed next_auth from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
-import { SessionProvider, signIn, signOut, useSession} from 'next-auth/react'
 
 export default function Navbar() {
 
-  const {data:session, status} = useSession();
   
   return (
     <nav className='header'>
       <h1 className='logo'>
         <a href='#'>Golf Weather</a>
       </h1>
-      <ul className={`'main-nav'${
-          !session && status === 'loading' ? 'loading' : 'loaded'
-        }`}>
-         {!session && status !== 'authenticated' &&(
+      <ul className='main-nav' />
           <li>
-          <Link href='/api/auth/signin'>
-              <a onClick={e => {
-                signIn('github')
-              }}>
-                Sign In
-              </a>
-              </Link>
-          </li>
-        )}
-         {session && status !== 'unauthenticated' && (
+          <Link href='/api/auth/signin'></Link>
+          </li>   
           <li>
-          <Link href='/api/auth/signout'>
-            <a onClick={e => {
-              signOut()
-            }}>
-              Sign Out
-            </a>
-            </Link>
-        </li>
-        )}    
-        </ul>
+          <Link href='/api/auth/signout'></Link>
+        </li>  
     </nav>
   )
 }
